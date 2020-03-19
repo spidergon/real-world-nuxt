245f7d7a4883b26b413d03ccddbd6423151a5d98
Christopher Servius <spidergon@gmail.com>
2020-03-19 11:46:29 -0300

    dev

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,20 @@
 import NavBar from '~/components/NavBar.vue'
 
 export default {
-  components: { NavBar }
+  components: { NavBar },
+  head() {
+    return {
+      titleTemplate: '%s - Real World Events',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content:
+            'Where you can find all the events taking place in your neighborhood'
+        }
+      ]
+    }
+  }
 }
 </script>
 
